d324ee6fdce6a80b1b366c9a753ea31c11245305
muhammadqamar <muhammadqamar111@gmail.com>
2022-10-20 20:14:34 +0500

    added header

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,3 +1,7 @@
+import "../assets/style/main.scss";
+import "../assets/style/header.scss";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
