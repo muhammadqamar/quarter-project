8df703f7c55467fa19e73e77218f1761d12f88c7
muhammadqamar <muhammadqamar111@gmail.com>
2022-11-03 15:39:29 +0500

    added contentful

---
+++ src/contentfulApi.js
@@ -0,0 +1,34 @@
+import { createClient } from "contentful";
+
+const client = createClient({
+  space: process.env.CONTENTFUL_SPACE_ID, // ID of a Compose-compatible space to be used \
+  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN, // delivery API key for the space \
+});
+
+export async function fetchEntries({ pageContentType, locale, limit }, query) {
+  try {
+    const entries = await client.getEntries({
+      content_type: pageContentType,
+      locale,
+      ...query,
+      limit,
+    });
+
+    if (entries.items) {
+      return entries.items;
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function fetchHome(locale) {
+  const entries = await fetchEntries({
+    contentType: "meet",
+    locale,
+  });
+
+  const docs = entries?.map((entry) => ({ ...entry.fields })) || [];
+
+  return docs;
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,23 @@
-import Head from "next/head";
-import Image from "next/image";
+import { GetStaticPropsContext } from "next";
 import Layout from "../components/Layout";
 import HomePage from "../components/HomePage/index.js";
+import { fetchHome } from "../contentfulApi";
 
-export default function Home() {
+export default function Home(homePage) {
+  console.log(homePage);
   return (
     <Layout>
       <HomePage />
     </Layout>
   );
+}
+
+export async function getStaticProps(context) {
+  const homePage = await fetchHome(context.locale);
+
+  return {
+    props: {
+      homePage,
+    },
+  };
 }
